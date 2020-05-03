8ec3c26a3ced3ecc276ef2bf36d2226c0f59aac7
Manuel Quinones <manuel.quinones@target.com>
2020-05-03 14:05:44 -0500

    final step 3

--- vehicle.go
+++ vehicle.go
@@ -104,7 +104,7 @@ func main() {
 }
 
 func showRating(model string) {
-	var ratingFound bool = false
+	ratingFound := false
 	for m, r := range vehicleResult {
 		if m == model {
 			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
