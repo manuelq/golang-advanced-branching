419144f1f6932e99f66dc9db07308b23dcc0825d
Manuel Quinones <manuel.quinones@target.com>
2020-05-03 13:31:06 -0500

    commit

--- vehicle.go
+++ vehicle.go
@@ -111,7 +111,7 @@ func generateRating() {
 		var vehRating rating
 		for _, msg := range v.Feedback {
 			if text := strings.Split(msg, " "); len(text) >= 5 {
-				vehRating := 5
+				vehRating = 5
 				vehResult.feedbackTotal++
 				for _, word := range text {
 
